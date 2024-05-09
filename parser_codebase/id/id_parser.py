e3db7f918bebab89946abff6ebb3bfb3e6a0c474
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2024-05-09 09:59:35 +0530

    updated values for id parsing doctype

--- parser_codebase/id/id_parser.py
+++ parser_codebase/id/id_parser.py
@@ -1,5 +1,3 @@
-#%%
-# code for id parser with country values
 import csv
 import xml.etree.ElementTree as ET
 
@@ -7,6 +5,7 @@ import xml.etree.ElementTree as ET
 xml_fp = r"C:\Users\kadam\OneDrive\Documents\GitHub\WLF_Tuning_OFAC_Parser_Dashboard\source_documents\sdn_advanced.xml"
 output_fp = "C:/Users/kadam/OneDrive/Documents/GitHub/WLF_Tuning_OFAC_Parser_Dashboard/parser_codebase/id/output_id_with_countryvalues.csv"
 country_values_fp = "C:/Users/kadam/OneDrive/Documents/GitHub/WLF_Tuning_OFAC_Parser_Dashboard/parser_codebase/id/countryvalues.txt"  # Assuming this file contains the <CountryValues> XML data
+doc_type_values_fp = "C:/Users/kadam/OneDrive/Documents/GitHub/WLF_Tuning_OFAC_Parser_Dashboard/parser_codebase/id/doctypevalues.txt"  # Assuming this file contains the <IDRegDocTypeValues> XML data
 
 # Parse the XML document for country values
 country_tree = ET.parse(country_values_fp)
@@ -19,6 +18,17 @@ for country in country_root.findall(".//Country"):
     country_name = country.text
     country_mapping[country_id] = country_name
 
+# Parse the XML document for document type values
+doc_type_tree = ET.parse(doc_type_values_fp)
+doc_type_root = doc_type_tree.getroot()
+
+# Create a mapping from Document Type ID to Document Type Name
+doc_type_mapping = {}
+for doc_type in doc_type_root.findall(".//IDRegDocType"):
+    doc_type_id = doc_type.attrib["ID"]
+    doc_type_name = doc_type.text
+    doc_type_mapping[doc_type_id] = doc_type_name
+
 # Parse the main XML document
 tree = ET.parse(xml_fp)
 root = tree.getroot()
@@ -30,9 +40,11 @@ ns = {"ns": "http://www.un.org/sanctions/1.0"}
 with open(output_fp, "w", newline="", encoding="utf-8-sig") as csvfile:
     fieldnames = [
         "FixedRef",
-        "Document_Type",
+        "Document_Type_ID",
+        "Document_Type_Name",
         "Issued_By",
-        "Issuing Country",
+        "Issuing_Country_ID",
+        "Issuing_Country_Name",
         "Issue_Date",
         "Expiration_Date",
         "Value",
@@ -49,10 +61,11 @@ with open(output_fp, "w", newline="", encoding="utf-8-sig") as csvfile:
 
         if distinct_party is not None:
             fixed_ref = distinct_party.attrib["FixedRef"]
-            document_type = idregdocument.attrib["IDRegDocTypeID"]
+            document_type_id = idregdocument.attrib["IDRegDocTypeID"]
+            document_type_name = doc_type_mapping.get(document_type_id, "Unknown Document Type")
             issued_by = idregdocument.find(".//ns:IssuingAuthority", ns).text if idregdocument.find(".//ns:IssuingAuthority", ns) is not None else ""
             issued_by_country_id = idregdocument.attrib.get("IssuedBy-CountryID", "")
-            issued_by_country_name = country_mapping.get(issued_by_country_id, "Unknown Country")  # Look up country name
+            issued_by_country_name = country_mapping.get(issued_by_country_id, "Unknown Country")
             value = idregdocument.find(".//ns:IDRegistrationNo", ns).text if idregdocument.find(".//ns:IDRegistrationNo", ns) is not None else ""
 
             # Initialize date variables
@@ -90,11 +103,14 @@ with open(output_fp, "w", newline="", encoding="utf-8-sig") as csvfile:
             # Write the data to the CSV file
             data = {
                 "FixedRef": fixed_ref,
-                "Document_Type": document_type,
+                "Document_Type_ID": document_type_id,
+                "Document_Type_Name": document_type_name,
                 "Issued_By": issued_by,
-                "Issuing Country": issued_by_country_name,
+                "Issuing_Country_ID": issued_by_country_id,
+                "Issuing_Country_Name": issued_by_country_name,
                 "Issue_Date": issue_date,
                 "Expiration_Date": expiration_date,
                 "Value": value,
             }
             writer.writerow(data)
+#%%
